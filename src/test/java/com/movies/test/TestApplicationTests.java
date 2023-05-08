7a48bc106a6c015ee8e6498b63e5c794df101141
mohit <mohit.bhagwani@synoriq.in>
2023-05-08 22:00:44 +0530

    Initial commit

---
+++ src/test/java/com/movies/test/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movies.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
